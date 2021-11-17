41c5d9eba6eb560636233fd473730417fafac1bc
Gohan1233 <gar21352@uvg.edu.gt>
2021-11-16 23:18:27 -0600

    Final Telefono

--- MTel.java
+++ MTel.java
@@ -1,3 +1,12 @@
+/*
+ * Nombre del programadores: Dolan Cuellar 21965, Gabriel García 21352, Ricardo Mendez 21289
+ * Contacto: cue21965@uvg.edu. gt, gar21352@uvg.edu.gt, men21289@uvg.edu.gt
+ * Nombre del programa: Cancion.java, Carro.java, Controlador.java, Mproductividad.java, MRadio.java, 
+                        MReproductor.java, MTel.java, TipoA.java, TipoB.java, Tipo C.java, TipoS.java, Vista.java
+ * Herramientas: Sublime Text 3, Visual Studio Code, IntelliJ IDEA
+ * Fecha de creación: 08/11/2021
+ * Fecha de finalización: 16/11/2021
+ */
 public interface MTel {
     String tel = "11225599";
     boolean conectado = true;
